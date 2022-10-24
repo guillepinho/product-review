ab22076fd6b1b4ebcdb3423232cf1b494cd80f67
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-10-23 23:56:07 -0300

    ✨ Done challenge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import iconCart from './images/icon-cart.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="body">
+      <div className="product">
+        <div className="img-div" />
+        <div className="product-info">
+          <span className="product-type">P E R F U M E</span>
+          <span className="product-title">Gabrielle Essence Eau De Parfum</span>
+          <span className="product-desc">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfume-Creator for the House of CHANEL.</span>
+          <div className="prices">
+            <span className="promo-price">$149.99</span>
+            <span className="normal-price">$169.99</span>
+          </div>
+          <button type="button" onClick={ () => console.log('Hello there!') } className="button">
+            <img src={ iconCart } alt="iconCart" />
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
